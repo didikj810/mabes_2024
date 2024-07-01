6d2e58da2d50e3936adaeb741d5686e6ce81f1c0
didikj810 <didikjhauhari810@gmail.com>
2024-07-01 12:12:26 +0700

    ubah file

--- latihan1.php
+++ latihan1.php
@@ -8,4 +8,6 @@ echo "Hello World";
 //3. git push -u origin master
 //push adalah perintah, origin adalah nama remote
 //master adalah nama branch
+//4. ini adalah modify dari saya
+//5. dengan menambahkan file baru
 ?>
